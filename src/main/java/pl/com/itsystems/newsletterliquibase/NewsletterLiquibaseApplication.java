98c26e680edf721fefbea8ffbf2fb50cbcffcaf9
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2024-06-23 18:52:21 +0200

    Create app

---
+++ src/main/java/pl/com/itsystems/newsletterliquibase/NewsletterLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package pl.com.itsystems.newsletterliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsletterLiquibaseApplication.class, args);
+    }
+
+}
